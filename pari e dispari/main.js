21ed47490408dd71b84985a9905847fe50f575ca
salvatorebriga <salvo.z2302@gmail.com>
2024-05-07 19:49:38 +0200

    [fix] input check

--- pari e dispari/main.js
+++ pari e dispari/main.js
@@ -1,9 +1,13 @@
 "use strict"
 
+/*dichiarazione parametri utente*/
 let userChoice = ' ';
+let userNumber = 0;
 
+/*variabile di supporto per i check dell'input*/
 let input = false;
 
+/*controllo sul pari o dispari*/
 do{
 
     userChoice = prompt('Scegli pari o dispari');
@@ -14,6 +18,26 @@ do{
 
 }while(input == false);
 
+/*reset della variabile di supporto*/
+input = false;
 
+
+/*controllo sul numero dell'utente*/
+do{
+
+    userNumber = Number(prompt('inserisci un numero da 1 a 5'));
+
+    if(isNaN(userNumber)){
+        console.log('Perfavore inserisci un numero');
+    }else if(userNumber % 1 !== 0){
+        console.log('Perfavore inserisci solo valori interi compresi fra 1 e 5');
+    }else if(userNumber >= 1 && userNumber <=5){
+        input = true;
+        console.log(userNumber);
+    }else{
+        console.log('Inserisci un numero compreso da 1 a 5');
+    }
+
+}while(input == false);
 
 
