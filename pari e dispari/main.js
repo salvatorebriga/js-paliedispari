738ae9d121bcbe8a79f774d07b79cfadfb720871
salvatorebriga <salvo.z2302@gmail.com>
2024-05-07 20:07:20 +0200

    added myEvenOrOddCheck function

--- pari e dispari/main.js
+++ pari e dispari/main.js
@@ -18,6 +18,8 @@ do{
 
 }while(input == false);
 
+console.log('Scelta utente: ' + userChoice);
+
 /*reset della variabile di supporto*/
 input = false;
 
@@ -33,11 +35,33 @@ do{
         console.log('Perfavore inserisci solo valori interi compresi fra 1 e 5');
     }else if(userNumber >= 1 && userNumber <=5){
         input = true;
-        console.log(userNumber);
+        console.log('Numero utente: ' + userNumber);
     }else{
         console.log('Inserisci un numero compreso da 1 a 5');
     }
 
 }while(input == false);
+
+myEvenOrOddCheck(userChoice, userNumber);
+
+function myEvenOrOddCheck(functionChoice, functionNumber){
+    let computerNumber = Math.floor(Math.random() * (5 - 1) + 1);
+    console.log('Numero del computer: ' + computerNumber);
+
+    let result = userNumber + computerNumber;
+
+    if(result % 2 === 0 && userChoice === 'pari'){
+        console.log(result + " PARI!!! Vince l'utente")
+    }else if(result % 2 === 1 && userChoice === 'dispari'){
+        console.log(result + " DISPARI!!! Vince l'utente")
+    }else{
+        if(result % 2 === 0){
+            console.log(result + " PARI!!! Vince il computer");
+        }else{
+            console.log(result + " DISPARI!!! Vince il computer");
+        }
+    }
+    
+}
 
 
