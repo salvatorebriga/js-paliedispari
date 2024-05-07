e7d9671f2b2a415893fdab120c4725f325637689
salvatorebriga <salvo.z2302@gmail.com>
2024-05-07 19:34:24 +0200

    even or odd input check

--- pari e dispari/main.js
+++ pari e dispari/main.js
@@ -1,1 +1,19 @@
 "use strict"
+
+let userChoice = ' ';
+
+let input = false;
+
+do{
+
+    userChoice = prompt('Scegli pari o dispari');
+
+    if(userChoice === 'pari' || userChoice === 'dispari'){
+        input = true;
+    }
+
+}while(input == false);
+
+
+
+
