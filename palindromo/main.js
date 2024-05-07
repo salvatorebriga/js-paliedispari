c55d26d268ce99456d9a2c283ed08b0b0a5f8d0e
salvatorebriga <salvo.z2302@gmail.com>
2024-05-07 19:19:24 +0200

    solution

--- palindromo/main.js
+++ palindromo/main.js
@@ -1,1 +1,25 @@
 "use strict"
+
+let input = prompt('inserisci la parola');
+
+input = input.replaceAll(' ','')
+input = input.toLowerCase();
+
+console.log(input);
+
+myPalindromeCheck(input);
+
+function myPalindromeCheck(userWord){
+    const splitWord = userWord.split("");
+    console.log(splitWord);
+    const reverseWord = splitWord.reverse();
+    console.log(reverseWord);
+    const joinWord = reverseWord.join("");
+    console.log(joinWord);
+
+    if(userWord === joinWord){
+        console.log('La parola è palindroma');
+    }else{
+        console.log('La parola non è palindroma');
+    }
+}
